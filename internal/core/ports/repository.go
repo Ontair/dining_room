194aa784a71d9fab87cd0477e2660341ac550d9d
Ontair <lushkin00@inbox.ru>
2025-08-20 15:02:06 +0300

    feat: работа с памятью in-memory

--- internal/core/ports/repository.go
+++ internal/core/ports/repository.go
@@ -1,7 +1,10 @@
 // ДАННЫЕ (как получить и сохранить).
 package ports
 
+import "github.com/Ontair/dining-room/internal/domain"
 
-type DiningRepository interface{
 
+type DishesRepository interface{
+	GetAll() ([]*domain.Dish, error)
+	CreateDishes(dish *domain.Dish) error
 }
---
+++ internal/infra/memory/memory.go
@@ -0,0 +1,47 @@
+package memory
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/Ontair/dining-room/internal/core/ports"
+	"github.com/Ontair/dining-room/internal/domain"
+)
+
+var _ ports.DishesRepository = (*MemoryDishesRepository)(nil)
+
+var (
+	ErrIsExist = errors.New("ID is already in the map")
+)
+
+type MemoryDishesRepository struct {
+	dishes map[string]*domain.Dish // map[string]*domain.
+	mux    sync.RWMutex
+}
+
+func NewMemoryDishesRepository() *MemoryDishesRepository {
+	return &MemoryDishesRepository{
+		dishes: make(map[string]*domain.Dish),
+	}
+}
+
+func (d *MemoryDishesRepository) GetAll() ([]*domain.Dish, error) {
+	d.mux.Lock()
+	defer d.mux.Unlock()
+	res := make([]*domain.Dish, len(d.dishes))
+	for _, dish := range d.dishes {
+		res = append(res, dish)
+	}
+	return res, nil
+}
+
+func (d *MemoryDishesRepository) CreateDishes(dish *domain.Dish) error{
+	d.mux.Lock()
+	defer d.mux.Unlock()
+
+	if i := d.dishes[dish.ID]; i!=nil{
+		return ErrIsExist
+	}
+	d.dishes[dish.ID] = dish
+	return nil
+}
